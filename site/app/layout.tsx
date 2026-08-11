c21be9e96123e49953acca0bc2809e036bd6fe8b
Brett Connor <brettcon.cisco@proton.me>
2026-08-11 23:59:43 +0000

    Sync from specpubspec-dev (2026-08-11T23:59:43Z)

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: 'Seed Doc',
   description:
     'A static rendering of this repository\'s canonical specification document.',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
